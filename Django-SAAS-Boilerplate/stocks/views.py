81c80161cb210e1b18be313b0f12db5e14cb5836
kaditya67 <322adityakumar0004@dbit.in>
2025-01-31 15:53:24 +0530

    Landing page improved

--- Django-SAAS-Boilerplate/stocks/views.py
+++ Django-SAAS-Boilerplate/stocks/views.py
@@ -293,6 +293,8 @@ def profile(request):
     return render(request, 'stocks/stock_users/profile.html', context)
 
 def main_page(request):
+    # if request.user.is_authenticated:
+    #     return redirect('dashboard')
     return render(request,'stocks/main_page.html')
 
 def sidebar(request):
